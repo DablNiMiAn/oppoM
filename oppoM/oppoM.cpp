baf8ea88b3c4298a1dfb574d5a7afbc0bbcc124b
krintwastaken <gamerofthe21thcentury@gmail.com>
2024-10-11 11:21:06 +0700

    Update oppoM.cpp

--- oppoM/oppoM.cpp
+++ oppoM/oppoM.cpp
@@ -1,32 +1,30 @@
-﻿#include <sstream>  
+#include <sstream>  
 #include <iostream>
 #include <fstream>  
 #include <string>  
 #include <ctime>  
 #include <iomanip>  
 #include <vector>  
-#include <algorithm>  
-
-using namespace std;
+#include <algorithm>
 
 class files {
 private:
-    string file_name;
+    std::string file_name;
     tm creation_date; // Используем tm для хранения даты  
     int size;
 
 public:
-    files(const string& filename, const tm& creation_date, int size)
+    files(const std::string& filename, const tm& creation_date, int size)
         : file_name(filename), creation_date(creation_date), size(size) {}
 
-    void display(ostream& os) const { // Используем ostream для переменной вывода  
+    void display(std::ostream& os) const { // Используем ostream для переменной вывода  
         os << "Имя файла: " << file_name << "\t";
-        os << "Дата создания: " << put_time(&creation_date, "%Y.%m.%d") << "\t";
-        os << "Размер: " << size << " Байт" << endl;
+        os << "Дата создания: " << std::put_time(&creation_date, "%Y.%m.%d") << "\t";
+        os << "Размер: " << size << " Байт" << std::endl;
     }
 
 
-    const string& getFilename() const {
+    const std::string& getFilename() const {
         return file_name;
     }
 
@@ -39,22 +37,22 @@ public:
     }
 };
 
-tm parseDate(const string& dateStr) {
+tm parseDate(const std::string& dateStr) {
     tm date = {};
-    istringstream ss(dateStr);
-    ss >> get_time(&date, "%Y.%m.%d");
+    std::istringstream ss(dateStr);
+    ss >> std::get_time(&date, "%Y.%m.%d");
     if (ss.fail()) {
-        throw runtime_error("Неправильный формат даты");
+        throw std::runtime_error("Неправильный формат даты");
     }
     return date;
 }
 
-files createfiles(const string& description) {
-    istringstream stream(description);
-    string filename;
-    string dateStr;
+files createfiles(const std::string& description) {
+    std::istringstream stream(description);
+    std::string filename;
+    std::string dateStr;
     int size = 0;
-    string temp;
+    std::string temp;
 
     bool filenameSet = false;
     bool dateSet = false;
@@ -65,7 +63,7 @@ files createfiles(const string& description) {
             filename = temp.substr(1, temp.size() - 2); // Удаляем кавычки   
             filenameSet = true;
         }
-        else if (temp.find('.') != string::npos && temp.size() <= 10) {
+        else if (temp.find('.') != std::string::npos && temp.size() <= 10) {
             dateStr = temp;
             dateSet = true;
         }
@@ -76,7 +74,7 @@ files createfiles(const string& description) {
     }
 
     if (!filenameSet or !dateSet or !sizeSet or size <= 0) {
-        throw runtime_error("Invalid object description");
+        throw std::runtime_error("Invalid object description");
     }
 
     tm creation_date = parseDate(dateStr);
@@ -85,36 +83,36 @@ files createfiles(const string& description) {
 
 int main() {
     setlocale(0, "ru");
-    ifstream inputFile("in.txt");
+    std::ifstream inputFile("in.txt");
     if (!inputFile.is_open()) {
-        cerr << "Error: Не удалось открыть файл 'in.txt'.\n";
+        std::cerr << "Error: Не удалось открыть файл 'in.txt'.\n";
         return 1;
     }
 
-    vector<files> fileList;
-    string description;
+    std::vector<files> fileList;
+    std::string description;
 
     while (getline(inputFile, description)) {
         try {
             files obj = createfiles(description);
             fileList.push_back(obj);
         }
-        catch (const exception& e) {
-            cerr << "Error: " << e.what() << "\n";
+        catch (const std::exception& e) {
+            std::cerr << "Error: " << e.what() << "\n";
         }
     }
 
     inputFile.close();
 
     int choice;
-    cout << "Выберите метод сортировки:\n";
-    cout << "1)По размеру файла (largest to smallest)\n";
-    cout << "2) По дате создания\n";
-    cout << "3) Без сортировки\n";
-    cout << "4) Вывод по временному промежутку\n";
-    cout << "5) Вывод по промежутку размеров\n";
-    cout << "Выберите цифру (1-5): ";
-    cin >> choice;
+    std::cout << "Выберите метод сортировки:\n"
+        << "1)По размеру файла (largest to smallest)\n"
+        << "2) По дате создания\n"
+        << "3) Без сортировки\n"
+        << "4) Вывод по временному промежутку\n"
+        << "5) Вывод по промежутку размеров\n"
+        << "Выберите цифру (1-5): ";
+    std::cin >> choice;
 
     switch (choice) {
     case 1:
@@ -122,7 +120,7 @@ int main() {
             return a.getSize() < b.getSize(); // Сортировка по размеру от большего к меньшему  
             });
         for (const auto& file : fileList) {
-            file.display(cout); // Выводим на экран  
+            file.display(std::cout); // Выводим на экран  
         }
         break;
     case 2:
@@ -130,28 +128,28 @@ int main() {
             return mktime(const_cast<tm*>(&a.getCreationDate())) < mktime(const_cast<tm*>(&b.getCreationDate())); // Сортировка по дате создания  
             });
         for (const auto& file : fileList) {
-            file.display(cout); // Выводим на экран  
+            file.display(std::cout); // Выводим на экран  
         }
         break;
     case 3:
         for (const auto& file : fileList) {
-            file.display(cout); // Выводим на экран  
+            file.display(std::cout); // Выводим на экран  
         }
         // Выводим в том порядке, в каком есть  
         break;
     case 4:
     {
-        string start_date_str, end_date_str;
-        cout << "Введите начальную дату (ГГГГ.ММ.ДД): ";
-        cin >> start_date_str;
-        cout << "Введите конечную дату (ГГГГ.ММ.ДД): ";
-        cin >> end_date_str;
+        std::string start_date_str, end_date_str;
+        std::cout << "Введите начальную дату (ГГГГ.ММ.ДД): ";
+        std::cin >> start_date_str;
+        std::cout << "Введите конечную дату (ГГГГ.ММ.ДД): ";
+        std::cin >> end_date_str;
 
         tm start_date = parseDate(start_date_str);
         tm end_date = parseDate(end_date_str);
 
         // Фильтрация файлов по дате
-        vector<files> filteredFileList;
+        std::vector<files> filteredFileList;
         for (const auto& file : fileList) {
             tm fileDate = file.getCreationDate();
             if (mktime(&fileDate) >= mktime(&start_date) && mktime(&fileDate) <= mktime(&end_date)) {
@@ -161,12 +159,12 @@ int main() {
 
         // Вывод отфильтрованных файлов
         if (filteredFileList.empty()) {
-            cout << "Файлов, созданных в указанном диапазоне дат, нет.\n";
+            std::cout << "Файлов, созданных в указанном диапазоне дат, нет.\n";
         }
         else {
-            cout << "Список файлов, созданных в диапазоне дат " << start_date_str << " - " << end_date_str << ":" << endl;
+            std::cout << "Список файлов, созданных в диапазоне дат " << start_date_str << " - " << end_date_str << ":" << std::endl;
             for (const auto& file : filteredFileList) {
-                file.display(cout);
+                file.display(std::cout);
             }
         }
         break;
@@ -174,13 +172,13 @@ int main() {
     case 5:
     {
         int start_size, end_size;
-        cout << "Введите начальный размер файла: ";
-        cin >> start_size;
-        cout << "Введите конечный размер файла: ";
-        cin >> end_size;
+        std::cout << "Введите начальный размер файла: ";
+        std::cin >> start_size;
+        std::cout << "Введите конечный размер файла: ";
+        std::cin >> end_size;
 
         // Фильтрация файлов по дате
-        vector<files> filteredFileList;
+        std::vector<files> filteredFileList;
         for (const auto& file : fileList) {
             int size = file.getSize();
             if ((size) >= (start_size) && (size) <= (end_size)) {
@@ -190,18 +188,18 @@ int main() {
 
         // Вывод отфильтрованных файлов
         if (filteredFileList.empty()) {
-            cout << "Файлов, созданных в указанном диапазоне размеров, нет.\n";
+            std::cout << "Файлов, созданных в указанном диапазоне размеров, нет.\n";
         }
         else {
-            cout << "Список файлов, созданных в диапазоне размеров " << start_size << " - " << end_size << ":" << endl;
+            std::cout << "Список файлов, созданных в диапазоне размеров " << start_size << " - " << end_size << ":" << std::endl;
             for (const auto& file : filteredFileList) {
-                file.display(cout);
+                file.display(std::cout);
             }
         }
         break;
     }
     default:
-        cout << "Такого числа нет в списке.\n";
+        std::cout << "Такого числа нет в списке.\n";
         return 1;
     }
     return 0;
